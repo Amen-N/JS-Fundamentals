8480b6dc29e93bdd45893654b03e2a3dfe938ea1
Amen N. <amen4nigussie@gmail.com>
2025-07-06 23:18:59 +0300

    4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -2,3 +2,31 @@ const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
 console.log('${arg1} is ${arg2}');
+
+const lines = ["C is fun", "Python is cool", "Javascript is amazing"];
+for (const line of lines) {
+  console.log(line);
+}
+
+const x = parseint(process.argv[2]};
+
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < x; i++) console.log("C is fun");
+}
+
+const size = parsInt(process.argv[2]);
+if (isNaN(size)){ 
+console.log("Missing size");
+} else }
+for (let i=0; i<size; i++) {
+  console.log("x".repeat(size));}
+}
+function add(a,b) {
+  return a+b;
+}
+const a = parseInt(process.argv[2]);
+const b =parseInt(process.argv[3]);
+
+console.log(add(a,b));
